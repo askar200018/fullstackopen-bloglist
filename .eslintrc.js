c14c84a2ea586c6d138cdd25dfec798ed70f4731
askar-dev <askar200018@mail.ru>
2022-05-31 16:20:53 +0600

    4.2 Blog list, step2

---
+++ .eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+  'env': {
+    'commonjs': true,
+    'es2021': true,
+    'node': true
+  },
+  'extends': 'eslint:recommended',
+  'parserOptions': {
+    'ecmaVersion': 'latest'
+  },
+  'rules': {
+    'indent': [
+      'error',
+      2
+    ],
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'semi': [
+      'error',
+      'never'
+    ],
+    'eqeqeq': 'error',
+    'no-trailing-spaces': 'error',
+    'object-curly-spacing': [
+      'error', 'always'
+    ],
+    'arrow-spacing': [
+      'error', { 'before': true, 'after': true }
+    ],
+    'array-bracket-spacing': ['error', 'never'],
+    'no-console': 0,
+    'eol-last': ['error', 'always']
+  }
+}
+
